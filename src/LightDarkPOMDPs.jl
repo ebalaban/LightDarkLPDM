697e8d3a9c102e9a0afe55af4fffad998f016ffb
Zachary Sunberg <sunbergzach@gmail.com>
2017-01-21 19:41:08 -0800

    initial implementation and plotting works

--- src/LightDarkPOMDPs.jl
+++ src/LightDarkPOMDPs.jl
@@ -1,5 +1,14 @@
 module LightDarkPOMDPs
 
-# package code goes here
+importall POMDPs
+importall GenerativeModels
+
+using StaticArrays
+using Plots
+using POMDPToolbox
+
+export LightDark2D
+
+include("lightdark2d.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using LightDarkPOMDPs
 using Base.Test
+using POMDPs
+using POMDPToolbox
+using StaticArrays
+using Plots
 
-# write your own tests here
-@test 1 == 2
+p = LightDark2D(0.0, 5.0, diagm([0.5, 0.5]), diagm([0.5, 0.5]))
+pol = FunctionPolicy(s -> rand(2) - SVector(0.5,0.5))
+
+sim = HistoryRecorder(max_steps=20)
+
+h = simulate(sim, p, pol)
+
+plot(p, h)
+
+gui()
