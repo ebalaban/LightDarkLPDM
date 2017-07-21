c01da51ea5618ab2a6edf2cf156c98b6f454618a
tomerarnon <tomerarnon@users.noreply.github.com>
2017-07-20 19:54:49 -0700

    so far so broken

---
+++ src/LPDMBounds.jl
@@ -0,0 +1,99 @@
+import LPDM: bounds
+
+immutable LDBounds 
+end
+
+function LPDM.bounds(b::LDBounds,
+    pomdp::AbstractLD2,
+    particles::Vector{LPDMParticle{LDState}},
+    config::LPDMConfig)
+
+    ub = Array{Int8}(0);
+    lb = Array{Int8}(0);
+    for s in particles
+        push!(ub, upperBound(pomdp, s))
+        push!(lb, lowerBound(pomdp, s))
+    end
+
+    return minimum(lb), maximum(ub)
+end
+
+function lowerBound(p::LightDark2DTarget, particle::POMDPToolbox.Particle{Vec2})
+# computes the cost of traveling to the low noise region and only then towards target
+   #       s__________
+   #                  |
+   #                  |
+   #           x______|
+
+    s = particle.state
+    actions = POMDPs.actions(p, true);
+    r::Int8 = 0;
+    remx::Float64 = 0;
+    remy::Float64 = 0;
+    ##  Move x to low noise region
+    r1,remx = take_action(p.min_noise_loc-s[1], p.term_radius, actions)         # calculate cost for moving x to low noise region
+    r2,remy = take_action(s[2], p.term_radius, actions)                         # calculate cost for moving y to target coordinate
+    r3,remx = take_action(p.min_noise_loc-remx, p.term_radius, actions)         # calculate cost for moving x to target coordinate from where it reached in the low noise region
+
+    return -(r1 + r2 + r3)
+end
+lowerBound(p::LightDark2DTarget, particle::LPDM.LPDMParticle{StaticArrays.SVector{2,Float64}}) = lowerBound(p, POMDPToolbox.Particle{Vec2}(particle.state, particle.weight))
+
+
+function upperBound(p::LightDark2DTarget, particle::POMDPToolbox.Particle{Vec2})
+    # computes the reward for the straight-line path to target
+    s = particle.state
+    actions = POMDPs.actions(p, true);
+    rx::Int8 = 0
+    ry::Int8 = 0
+    remx::Float64 = 0
+    remy::Float64 = 0
+
+    rx,remx = take_action(s[1], p.term_radius, actions)        ##  for x: cost to move x in a straight line to within target region
+    ry,remy = take_action(s[2], p.term_radius, actions)        ##  same for y
+
+    r = rx > ry ? rx : ry                                  ## pick the larger of the two
+    return -r
+end
+upperBound(p::LightDark2DTarget, particle::LPDM.LPDMParticle{StaticArrays.SVector{2,Float64}}) = upperBound(p, POMDPToolbox.Particle{Vec2}(particle.state, particle.weight))
+
+
+# function take_action(x::Float64, terminal::Float64, actions::Array{Float64,1})
+#     r::Int8 = 0;
+#     x = abs(x);
+#     if x > terminal
+#         r = floor(x);               # largest available step is 1
+#         remx = x - r;
+#         if remx > terminal
+#             for a in actions
+#                 while remx > a && remx > terminal
+#                     remx -= a;
+#                     r += 1
+#                 end
+#             end
+#         end
+#     end
+#     return r
+# end
+
+function take_action(x::Float64, terminal::Float64, actions::Array{Float64,1})
+    r::Int8 = 0;
+    x = abs(x);
+    if x > terminal && terminal>minimum(actions)
+        for a in actions
+            if x > a && x > terminal
+                steps = floor(x/a)
+                x -= steps*a;
+                r += steps
+            end
+        end
+    elseif terminal < minimum(actions)
+        warn("""
+            The set of available actions will never allow x to arrive within the target radius
+                    x = $x
+                    terminal radius = $terminal
+                    actions = $actions
+            """)
+    end
+    return r, x
+end
---
+++ src/execute.jl
@@ -0,0 +1,126 @@
+
+# using Plots
+importall POMDPs
+
+using Combinatorics
+# using Plots
+using POMDPToolbox, Parameters, ParticleFilters, StaticArrays
+using LightDarkPOMDPs
+using LPDM
+import LPDM: init_bounds
+
+
+# Typealias appropriately
+typealias LDState   SVector{2,Float64}
+typealias LDAction  SVector{2,Float64}
+typealias LDObs     SVector{2,Float64}
+typealias LDBelief  LPDMBelief
+include("LPDMBounds.jl")
+
+
+function execute(n_sims::Int64 = 100)
+
+    p = LightDark2DTarget()
+    config = LPDMConfig();
+    config.n_particles = 100;
+    config.sim_len = 100;
+    p.n_rand = 2;
+    config.search_depth = 10;
+
+    state::LDState              = LDState(π, e);
+    rewards::Array{Float64}     = Array(Float64, 0)
+
+    bu = LPDMBeliefUpdater{LDState, LDAction, LDObs}(p, n_particles = config.n_particles);  # initialize belief updater
+    initial_states = state_distribution(p, state, config)                                   # create initial  distribution
+    current_belief = LPDM.create_belief(bu)                                                 # allocate an updated belief object
+
+    LPDM.initialize_belief(bu, initial_states, current_belief)                              # initialize belief
+    updated_belief = LPDM.create_belief(bu)                                                 # update belief now that it has been initialized
+    custom_bounds = LDBounds()                                                              # bounds object
+
+    solver = LPDMSolver{LDState,
+    LDAction,
+    LDObs,
+    LDBounds}(bounds = custom_bounds)
+    init_solver(solver, p)
+
+    policy::LPDMPolicy = POMDPs.solve(solver, p)
+    seed  ::UInt32   = convert(UInt32, 42)                          # the main random seed that's used to set the other seeds
+
+    rng = LPDMRandomVector([])                                      # random number vector
+    rs = FiniteRandomStreamsCommonSeed(1, 100, 1, seed)             # seed(?) for the random vector 
+
+    sim_steps::Int64 = 1
+    r::Float64 = 0.0
+
+    println(updated_belief)
+
+    tic() # start the clock
+    while !isterminal(p, state) && (solver.config.sim_len == -1 || sim_steps < solver.config.sim_len)
+        action = POMDPs.action(policy, current_belief)
+        println(action)
+        rng.vector = rs.streams[1,sim_steps,:]
+        state, obs, r = generate_sor(p, state, action, rng)
+        push!(rewards, r)
+        println(rewards)
+        # update belief
+        POMDPs.update(bu, current_belief, action, obs, updated_belief)
+        current_belief = deepcopy(updated_belief)
+        sim_steps += 1
+    end
+    run_time::Float64 = toq() # stop the clock
+
+    # Compute discounted reward
+    discounted_reward::Float64 = 0.0
+    multiplier::Float64 = 1.0
+    for r in rewards
+        discounted_reward += multiplier * r
+        multiplier *= p.discount
+    end
+
+    return sim_steps, sum(rewards), discounted_reward, run_time
+end
+
+
+
+
+
+############
+# place all of these where they belong once you figure out where that is.
+###########
+
+function LPDM.init_bounds(::LDBounds, ::AbstractLD2, ::LPDM.LPDMConfig)
+end
+
+
+function state_distribution(p::AbstractLD2, s::Vec2, config::LPDMConfig)
+    randx = randn(config.n_particles);
+    randy = randn(config.n_particles);
+    states = Vector{POMDPToolbox.Particle{Vec2}}();
+    particle = POMDPToolbox.Particle{Vec2}([0,0],1)
+
+    for rx in randx, ry in randy
+        particle = POMDPToolbox.Particle{Vec2}([s[1]+rx, s[2]+ry],1)
+        push!(states, particle)
+    end
+    return states
+end
+
+# LPDM specific random function:
+Base.rand(rng::LPDM.LPDMRandomVector, d::SymmetricNormal2) = d.mean + d.std*Vec2(Base.rand(rng.vector, 2))
+
+
+
+# #  check if UB is ever lower than LB
+# function foo(j::Int64 = 10000)                      
+#     iswrong = Array{LDState ,1}(0)
+#     for i in 1:j
+#         st = LDState(rand(2)*10)
+#         s = POMDPToolbox.Particle{Vec2}(st,1)
+#         ub, lb  = upperBound(p,s), lowerBound(p,s)
+#         if lb>ub
+#             push!(iswrong, st)
+#         end
+#     end
+#     return iswrong
+# end
