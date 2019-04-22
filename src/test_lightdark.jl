15296dad3ebeb780cf0dca771f4663db59618e56
ebalaban <ebalaban@stanford.edu>
2019-04-22 01:26:01 -0700

    Implemented an option to conveniently run the debugger configuration.

--- src/test_lightdark.jl
+++ src/test_lightdark.jl
@@ -1,6 +1,6 @@
 using Revise
 import LPDM, D3Trees, LightDarkPOMDPs, execute
 
-# execute.batch_execute(dims=1)
-# execute.batch_execute(dims=1)
-execute.batch_execute(dims=1)
+# execute.batch_execute(dims=2)
+# Juno.@run execute.batch_execute(dims=1, debugger=true)
+execute.batch_execute(dims=1, debugger=true)
