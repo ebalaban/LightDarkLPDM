6e28396e75bda82af20a7bbe6a98f4b52fffb98d
Logan Kilpatrick <23kilpatrick23@gmail.com>
2019-07-18 18:05:31 -0700

    Added a comment and question.

--- src/lightdark2dlpdm.jl
+++ src/lightdark2dlpdm.jl
@@ -105,7 +105,7 @@ function POMDPs.actions(p::LightDark2DLpdm, ::Bool)
 end
 
 # for tree construction
-# Action setup starts here, goes to LPDM.next_actions, and then to LPDM - solver.jl 
+# Action setup starts here, goes to LPDM.next_actions, and then to LPDM - solver.jl
 function POMDPs.actions(p::LightDark2DLpdm)
      if p.action_mode == :standard
          return p.standard_action_space
@@ -124,7 +124,7 @@ POMDPs.actions(p::LightDark2DLpdm, ::LD2State) = actions(p::LightDark2DLpdm)
 
 LPDM.max_actions(pomdp::LightDark2DLpdm) = pomdp.max_actions
 
-# For "simulated annealing"
+# For "simulated annealing" which is adaptive.
 # Uses POMDPs.actions and Solver.jl to create the action space.
 # Actual countinuous action space creation occurs dynamically in LPDM, not explicity in this code.
 function LPDM.next_actions(pomdp::LightDark2DLpdm,
@@ -147,6 +147,7 @@ function LPDM.next_actions(pomdp::LightDark2DLpdm,
             # return pomdp.extended_action_space   #DEBUG, let's try this...
             return pomdp.standard_action_space
         end
+        #NOTE: Why do we do this? the logic is not required... 
     end
 
     l_initial = length(pomdp.standard_action_space)
